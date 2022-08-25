8bcd93c99e9cfa3c88bcc28586151965a57040e3
andypayne <apayne@gmail.com>
2022-08-25 11:44:13 -0700

    Make gen_toc work on mac.

--- gen_toc.sh
+++ gen_toc.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 # Update the table of contents
 
-sed -i '0,/^<TOC>$/d' ./readme.md
+case `uname` in
+  Darwin)
+  sed -i '' '1,/^<TOC>$/d' ./readme.md
+  ;;
+  Linux)
+  sed -i '0,/^<TOC>$/d' ./readme.md
+  ;;
+esac
+
 HDR="# Dev Environment Cheatsheet"
 echo -e "${HDR}\n\n$(./node_modules/markdown-toc/cli.js ./readme.md)\n\n<TOC>\n$(cat ./readme.md)\n" > ./readme.md
 
